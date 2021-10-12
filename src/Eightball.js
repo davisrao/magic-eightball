77f51492f9f2bcd0f21a5ae18585407b3a9418a4
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-12 12:03:30 -0700

    completed Eightball and made tweeks from code review

--- src/Eightball.js
+++ src/Eightball.js
@@ -1,12 +1,44 @@
 import ANSWERS from './answers';
 import { useState } from 'react';
-import { choice } from './helpers'
+import { choice } from './helpers';
+import './Eightball.css';
 
-function Eightball({answers = ANSWERS}){
+/** Provides a random answer to your question
+ *  
+ *  Props: answers
+ * 
+ *  State: 
+ *      - message (string)
+ *      - color (string)
+ *  
+ *  App -> Eightball
+ */
+
+function Eightball({ answers=ANSWERS }){
     const [message,setMessage] = useState("Think of a question!");
     const [color, setColor] = useState("black");
+    const [answer, setAnswer] = useState({
+            msg: "Think of a question!",
+            color: "black",
+    });
 
-    //return html with defaults, on click choice of random color
-    // and random message
+    const style = {
+        backgroundColor: color,
+    }
+
+    function handleClick() {
+        const answer = choice(answers);
+        setMessage(answer.msg);
+        setColor(answer.color);
+        // setAnswer(answer.msg)
+    }
+
+    return (
+        <div style={style} className="Eightball" onClick={handleClick}>
+            <p className="Eightball-message">{message}</p>
+        </div>
+    )
 
 }
+
+export default Eightball;
--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,6 @@
-
+/** Takes in an array and returns a random item
+ *  within the array
+ */
 function choice (arr){
     let randIdx = Math.floor(Math.random()*arr.length);
     return arr[randIdx];
