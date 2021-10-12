dd186bf3f7f23d0b2d56376652ad12bc4c2cb42e
davisrao <davisrao1@gmail.com>
2021-10-12 12:16:18 -0700

    added reset button and minor refactor

--- src/Eightball.js
+++ src/Eightball.js
@@ -15,27 +15,34 @@ import './Eightball.css';
  */
 
 function Eightball({ answers=ANSWERS }){
-    const [message,setMessage] = useState("Think of a question!");
-    const [color, setColor] = useState("black");
-    const [answer, setAnswer] = useState({
-            msg: "Think of a question!",
-            color: "black",
-    });
+    const defaultAnswer = {msg:"Think of a question!",color: "black"};
+    // including set use state for message/color as alternate answer
+    // const [message,setMessage] = useState("Think of a question!");
+    // const [color, setColor] = useState("black");
+    const [answer, setAnswer] = useState(defaultAnswer);
 
     const style = {
-        backgroundColor: color,
+        backgroundColor: answer.color,
     }
 
     function handleClick() {
-        const answer = choice(answers);
-        setMessage(answer.msg);
-        setColor(answer.color);
-        // setAnswer(answer.msg)
+        // commented lines below are alternate solutions
+        // const answer = choice(answers);
+        // setMessage(answer.message);
+        // setColor(answer.color);
+        setAnswer(choice(answers))
+    }
+
+    function resetEightBall(){
+        setAnswer(defaultAnswer)
     }
 
     return (
+        <div>
         <div style={style} className="Eightball" onClick={handleClick}>
-            <p className="Eightball-message">{message}</p>
+            <p className="Eightball-message">{answer.msg}</p>
+        </div>
+        <button className="Eightball-button" onClick={resetEightBall}>Reset My Fortune</button>
         </div>
     )
 
